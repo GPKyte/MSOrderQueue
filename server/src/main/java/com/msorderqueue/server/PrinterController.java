3ad424aaa134c9485eb8ad6b6ce7e9713fbfa1dd
Gavin Kyte <gpkyte@gmail.com>
2018-03-20 20:58:09 -0400

    Add initial PrinterController class

---
+++ server/src/main/java/com/msorderqueue/server/PrinterController.java
@@ -0,0 +1,28 @@
+package com.msorderqueue.server;
+
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class PrinterController {
+    @Autowired
+    PrinterMongoRepository printerRepository;
+
+    @GetMapping("/printers")
+    public List<Printer> getAllPrinters() {
+        Sort sortByName = new Sort(Sort.Direction.DESC, "name");
+        return printerRepository.findAll(sortByName);
+    }
+
+    @PostMapping(value="/printers")
+    public Printer createPrinter(@Valid @RequestBody Printer printer) {
+        printer.setStatus("READY");
+        return printerRepository.save(printer);
+    }
+}
