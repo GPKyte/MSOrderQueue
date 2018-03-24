1aa2c44b187b6843ffc6a171180a44335adcf92c
Gavin Kyte <gpkyte@gmail.com>
2018-03-24 15:39:45 -0400

    Make User class

---
+++ server/src/main/java/com/msorderqueue/server/User.java
@@ -0,0 +1,35 @@
+package com.msorderqueue.server;
+
+import com.msorderqueue.server.Request;
+
+import java.util.ArrayList;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+/**
+ * @author Gavin Kyte
+ */
+@Data
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Role role;
+    private ArrayList<Request> requests;
+
+    public enum Role {REQUESTER, STAFF, ADMIN};
+
+    public User() {}
+
+    public User(String first, String last, Role role) {
+        this.first = first;
+        this.last = last;
+        this.role = role;
+        this.requests = new ArrayList<>();
+    }
+}
