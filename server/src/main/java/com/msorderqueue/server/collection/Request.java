912ba017fa177427023b69a4e0bfc2d5f6a22a18
Gavin Kyte <gpkyte@gmail.com>
2018-04-03 18:10:46 -0400

    Add timestamp to request

--- server/src/main/java/com/msorderqueue/server/collection/Request.java
+++ server/src/main/java/com/msorderqueue/server/collection/Request.java
@@ -4,6 +4,7 @@ package com.msorderqueue.server;
 import com.msorderqueue.server.RequestStatus;
 import com.msorderqueue.server.RequestItem;
 
+import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.ArrayList;
@@ -19,17 +20,19 @@ public class Request {
     private String id; // Make this a timestamp
     private String user; // References requester username
     private String comments;
+    private Date timestamp;
     private RequestStatus status;
     private boolean forClass;
     private ArrayList<RequestItem> requestItems;
 
     public Request() {}
 
-    public Request(String username, String comments, boolean forClass, ArrayList<RequestItem> requestItems) {
-        this.user = username;
+    public Request(String user, String comments, boolean forClass, ArrayList<RequestItem> requestItems) {
+        this.user = user;
         this.comments = comments;
         this.forClass = forClass;
         this.status = RequestStatus.ORDERED;
         this.requestItems = new ArrayList<>(requestItems);
+        this.timestamp = new Date();
     }
 }
