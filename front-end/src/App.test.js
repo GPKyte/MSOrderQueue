7b6537daee262c55e545897bfb86ed03c88b9a33
3montyjack <3montyjack@gmail.com>
2018-04-03 23:23:14 -0400

    This is the last one i promise

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Printer from "./Printer/Printer.js"
 
 // it("renders without crashing", () => {
 //   const div = document.createElement("div");
