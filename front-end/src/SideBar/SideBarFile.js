e6c37eaa3ca3f0e89a51111c54e42b22188dd32e
3montyjack <3montyjack@gmail.com>
2018-04-14 15:07:51 -0400

    Fixed the sidebar failing

--- front-end/src/SideBar/SideBarFile.js
+++ front-end/src/SideBar/SideBarFile.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-class Printer extends Component {
+class File extends Component {
   render() {
     var printerData = this.props.data;
     return (
@@ -8,11 +8,22 @@ class Printer extends Component {
         {/* This is the base printer Object that displays all the nessicary for the future releases. */}
         {printerData["fileName"]}
         <div />
-        <button className="btn btn-primary btn-sm disabled" aria-disabled="true">Delete</button>
-        <button className="btn btn-secondary btn-sm disabled" aria-disabled="true">Queue</button>
+        <button
+          id={printerData["id"]}
+          className="btn btn-primary btn-sm disabled"
+          aria-disabled="true"
+        >
+          Delete
+        </button>
+        <button
+          className="btn btn-secondary btn-sm disabled"
+          aria-disabled="true"
+        >
+          Queue
+        </button>
       </div>
     );
   }
 }
 
-export default Printer;
+export default File;
