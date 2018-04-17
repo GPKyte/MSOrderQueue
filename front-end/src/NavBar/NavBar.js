4e716f8237f3e1abd04337d2741784575004da56
3montyjack <3montyjack@gmail.com>
2018-04-17 13:05:23 -0400

    Fixed some stuff, have no idea what

--- front-end/src/NavBar/NavBar.js
+++ front-end/src/NavBar/NavBar.js
@@ -15,6 +15,7 @@ class NavBar extends Component {
 
   //This is the base printer object that displays all of the data needed on every printer
   onClick(event) {
+    console.log(event.target.name, event.target.value)
     if (event.target.name != null) {
       this.setState({ [event.target.name]: event.target.value });
     }
@@ -23,42 +24,63 @@ class NavBar extends Component {
   render() {
     return (
       <nav>
-        <nav className="navbar navbar-light bg-light">
-          <div className="navbar-nav">
-            <div className="navbar-brand">
-              <a
-                className="nav-item nav-link"
-                name="currentView"
-                value="printers"
-                onClick={this.onClick}
-              >
-                Printers
-              </a>
-              <a
-                className="nav-item nav-link"
-                name="currentView"
-                value="addPrinters"
-                onClick={this.onClick}
-              >
-                Add Printers
-              </a>
-              <a
-                className="nav-item nav-link"
-                name="currentView"
-                value="loginPage"
-                onClick={this.onClick}
-              >
-                Login
-              </a>
-              <a
-                className="nav-item nav-link"
-                name="currentView"
-                value="archive"
-                onClick={this.onClick}
-              >
-                Archive
-              </a>
-            </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand">MakerSpace Printer Queue</a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+
+            <button
+              className="nav-item button-link"
+              name="currentView"
+              value="printers"
+              onClick={this.onClick}
+            >
+              Printers
+            </button>
+            <button
+              className="nav-item button-link"
+              name="currentView"
+              value="addPrinters"
+              onClick={this.onClick}
+            >
+              Add Printers
+            </button>
+            <button
+              className="nav-item button-link"
+              name="currentView"
+              value="loginPage"
+              onClick={this.onClick}
+            >
+              Login
+            </button>
+            <a
+              className="nav-item nav-link"
+              name="currentView"
+              value="loginPage"
+              onClick={this.onClick}
+            >
+              Login
+            </a>
+            <button
+              className="nav-item button-link nav-link"
+              name="currentView"
+              value="archive"
+              onClick={this.onClick}
+            >
+              Archive
+            </button>
+          </ul>
           </div>
         </nav>
 
@@ -81,6 +103,7 @@ class RenderedView extends Component {
       case "archive":
         return <div>Isnt Implemented Yet</div>;
       default:
+        console.log(this.props.value)
         return <div>ERRRRROOOOORRRRR</div>;
     }
   }
