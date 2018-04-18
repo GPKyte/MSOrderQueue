8d59aafb97dfb87150f577379ef7a75983bed19c
Gavin Kyte <gpkyte@gmail.com>
2018-04-17 21:57:57 -0400

    Fix front-back connection for https

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,7 +7,7 @@ import NavBar from "./NavBar/NavBar.js";
 
 class App extends Component {
   render() {
-    const url = "http://localhost:8080";
+    const url = "https://localhost:8443";
     //This is a tepmorary page with all of the elements inside of it to get api calls working before we end up seperating them into where they need to be
     return (
       <div className="App Test">
