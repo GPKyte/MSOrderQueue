be4770d07cff160bc72474afc2da626c1177fb92
Gavin Kyte <gpkyte@gmail.com>
2018-04-18 22:00:51 -0400

    Change naming for make printer to fit conventions

--- front-end/src/Make Printers/MakePrinters.js
+++ front-end/src/Make Printers/MakePrinters.js
@@ -9,7 +9,7 @@ class MakePrinter extends Component {
       PrinterName: "Geoffrey",
       Model: "SomeModel",
       Brand: "MakerBot",
-      Status: "READY"
+      Status: "OPEN"
     };
     this.handleChange = this.handleChange.bind(this);
     this.onClick = this.onClick.bind(this);
@@ -106,9 +106,9 @@ class MakePrinter extends Component {
           value={this.state.Status}
           onChange={this.handleChange}
         >
-          <option value="READY">Ready</option>
+          <option value="OPEN">Open</option>
           <option value="BUSY">Busy</option>
-          <option value="FINISHED">Finished</option>
+          <option value="DONE">Done</option>
         </select>
         <div />
         <button onClick={this.onClick} className="btn">
